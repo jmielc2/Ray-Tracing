bb688cbf9bce4fd8c21035b48e2d68384004d5a6
jmielc2 <jmielc2@uic.edu>
2023-12-27 19:55:18 -0600

    defined general functions to be implemented by surfaces.

--- Ray-Tracing/Components/Surface.cpp
+++ Ray-Tracing/Components/Surface.cpp
@@ -1,2 +1,10 @@
 #include "../stdafx.h"
 #include "Surface.h"
+
+float Sphere::getIntersectionParam(const Ray& ray) const {
+	return -1.0f;
+}
+
+glm::vec3 Sphere::getNormal(const glm::vec3& point) const {
+	return glm::vec3(0.0f, 1.0f, 0.0f);
+}
--- Ray-Tracing/Components/Surface.h
+++ Ray-Tracing/Components/Surface.h
@@ -6,17 +6,22 @@
 class Surface;
 class Sphere;
 
+#include <glm.hpp>
 #include "Entity.h"
+#include "Ray.h"
 
 class Surface : public Entity {
 protected:
 public:
-
+	virtual float getIntersectionParam(const Ray& ray) const = 0;
+	virtual glm::vec3 getNormal(const glm::vec3& point) const = 0;
 };
 
 class Sphere : public Surface {
 protected:
 public:
+	virtual float getIntersectionParam(const Ray& ray) const;
+	virtual glm::vec3 getNormal(const glm::vec3& point) const;
 };
 
 #endif
